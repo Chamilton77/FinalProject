427e5af48cf637bb4b8dc7c52f2d4b0b1aa73fb0
Carson Hamilton <cjcham77@gmail.com>
2017-01-23 15:49:42 -0700

    added the fully functional email system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'resume#homepage'
+  post "/contact" => 'resume#sendMail'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
