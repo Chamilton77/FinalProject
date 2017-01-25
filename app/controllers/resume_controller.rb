4b3615d469f2fb572ff2cbf20ec81a67e0291b16
Carson Hamilton <cjcham77@gmail.com>
2017-01-25 15:40:43 -0700

    fixed email for heroku

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -3,7 +3,23 @@ class ResumeController < ApplicationController
   end
 
   def sendMail
-  	@mail = Pony.mail(:to =>'cjcham77@gmail.com', :via => :smtp, :from => params[:email], :subject => params[:subject], :body => params[:emailbody])
+	Pony.mail({
+		  :to => 'cjcham77@gmail.com',
+		  :from => params[:email],
+		  :subject => params[:subject],
+		  :body => params[:emailbody],
+		  :via => :smtp,
+		  :via_options => {
+		    :address              => 'smtp.gmail.com',
+		    :port                 => '587',
+		    :enable_starttls_auto => true,
+		    :user_name            => 'cjcahm77@gmail.com',
+		    :password             => ENV["EMAIL_PASSWORD"],
+		    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+		    :domain               => "gmail.com" # the HELO domain provided by the client to the server
+		  }
+		})
+  	# @mail = Pony.mail(:to =>'cjcham77@gmail.com', :via => :smtp, :from => params[:email], :subject => params[:subject], :body => params[:emailbody])
 	redirect_to root_path
   end
 end
