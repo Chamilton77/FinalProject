de3d974be3f74c6b9b2803b7c22061a620fc50da
Carson Hamilton <cjcham77@gmail.com>
2017-01-25 16:01:23 -0700

    email

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -6,18 +6,18 @@ class ResumeController < ApplicationController
 	Pony.mail({
 		  :to => 'cjcham77@gmail.com',
 		  :via => :smtp,
+		  :from => params[:email],
+		  :subject => params[:subject],
+		  :body => params[:emailbody],
 		  :via_options => {
 		    :address              => 'smtp.gmail.com',
 		    :port                 => '587',
 		    :enable_starttls_auto => true,
-		    :user_name            => 'cjcahm77@gmail.com',
+		    :user_name            => 'cjcham77@gmail.com',
 		    :password             => ENV["EMAIL_PASSWORD"],
 		    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
 		    :domain               => "gmail.com" # the HELO domain provided by the client to the server
 		  }
-		  :from => params[:email],
-		  :subject => params[:subject],
-		  :body => params[:emailbody],
 		})
   	# @mail = Pony.mail(:to =>'cjcham77@gmail.com', :via => :smtp, :from => params[:email], :subject => params[:subject], :body => params[:emailbody])
 	redirect_to root_path
